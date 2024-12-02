fca880478d807bb5b1d1dbffe59f5d0887ac82ed
Dhaya <sekarsekar5017@gmail.com>
2024-12-02 11:27:13 +0530

    update

--- React-Components/src/App.jsx
+++ React-Components/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { CameraProvider } from "./components/CameraContext";
 import { ControlPanel, Navbar,Canvas } from './components'
@@ -10,19 +9,5 @@ const App = () => (
     <Canvas />
   </CameraProvider>
 );
-=======
-import { ControlPanel,Navbar } from './components'
-import './App.css'
-import Popup from './components/Popup'
-const App = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <ControlPanel/>
-      <Popup/> 
-    </div>
-  )
-}
->>>>>>> popup
 
 export default App;
